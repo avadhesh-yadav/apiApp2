e366a92cfedb9dc2f06d08c5a373c8e30c39967f
Avadhesh Yadav <93024167+avadhesh-yadav@users.noreply.github.com>
2024-12-31 18:46:03 +0530

    Update apiApp.py

--- apiApp.py
+++ apiApp.py
@@ -7,7 +7,7 @@ import json
 app = Flask(__name__)
 
 # Enable CORS for any Salesforce instance (wildcard for Salesforce domains)
-CORS(app, origins=["https://*.lightning.force.com", "https://*.force.com","*"])
+CORS(app, origins=["https://*.lightning.force.com", "https://*.force.com"])
 
 @app.route('/decrypt', methods=['POST'])
 def decrypt():
@@ -28,12 +28,9 @@ def decrypt():
 
         # Decrypt the token
         decrypted_text = f.decrypt(encrypted_token.encode('utf-8')).decode('utf-8')
-        
-        # Assuming the decrypted text is a JSON string, parse it
-        decrypted_json = json.loads(decrypted_text)
 
-        # Return the decrypted data as JSON response
-        return jsonify(decrypted_json)
+        # Return the decrypted text as JSON
+        return jsonify({'decrypted': decrypted_text})
 
     except Exception as e:
         # Return error message if decryption fails
@@ -41,5 +38,4 @@ def decrypt():
 
 # Run the application (use host='0.0.0.0' when deploying to external server)
 if __name__ == '__main__':
-    # Set host='0.0.0.0' to make it accessible externally, and run on port 5000.
     app.run(debug=True, host='0.0.0.0', port=5000)
